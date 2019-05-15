40689ba27658824ee8f8a2138eabb61cf83033dc
Clint Miller <cmiller@leviathansecurity.com>
2019-05-15 14:22:55 -0700

    Added support to open files. Changed plugin to have a sidebar rather than a popup (popup went away when opening file).

--- src/securedrop-e2e.js
+++ src/securedrop-e2e.js
@@ -1,2 +1,14 @@
-document.body.style.border = "5px solid red";
+function loadFile(o) 
+{
+	var fr = new FileReader();
+	reader.onload = function(e)
+		{	
+		showDataFile(e, o);
+		};
+	fr.readAsText(o.files[0]);
+}
 
+function showDataFile(e, o)
+{
+	document.getElementById("data").innerText = e.target.result;
+}
