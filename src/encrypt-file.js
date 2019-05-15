0809002685192c7be832703df2c19a562412cc40
Corbin Souffrant <corbin.souffrant@leviathansecurity.com>
2019-05-15 16:49:08 -0700

    Including openpgp.js scripts from dist. Created a currently non-functioning encrypt-file.js that tries to encrypt 0x1,0x1,0x1. Added test.html wrapper to render the script in the browser.

---
+++ src/encrypt-file.js
@@ -0,0 +1,57 @@
+var openpgp = require('openpgp.js');
+//await openpgp.initWorker({ path:'openpgp.worker.js' });
+
+/*var pubkey =
+`-----BEGIN PGP PUBLIC KEY BLOCK-----
+-----END PGP PUBLIC KEY BLOCK-----`;
+*/
+
+// For now we are using the default key from openpgpjs
+var pubkey =
+  ['-----BEGIN PGP PUBLIC KEY BLOCK-----',
+  'Version: GnuPG v2.0.19 (GNU/Linux)',
+  '',
+  'mI0EUmEvTgEEANyWtQQMOybQ9JltDqmaX0WnNPJeLILIM36sw6zL0nfTQ5zXSS3+',
+  'fIF6P29lJFxpblWk02PSID5zX/DYU9/zjM2xPO8Oa4xo0cVTOTLj++Ri5mtr//f5',
+  'GLsIXxFrBJhD/ghFsL3Op0GXOeLJ9A5bsOn8th7x6JucNKuaRB6bQbSPABEBAAG0',
+  'JFRlc3QgTWNUZXN0aW5ndG9uIDx0ZXN0QGV4YW1wbGUuY29tPoi5BBMBAgAjBQJS',
+  'YS9OAhsvBwsJCAcDAgEGFQgCCQoLBBYCAwECHgECF4AACgkQSmNhOk1uQJQwDAP6',
+  'AgrTyqkRlJVqz2pb46TfbDM2TDF7o9CBnBzIGoxBhlRwpqALz7z2kxBDmwpQa+ki',
+  'Bq3jZN/UosY9y8bhwMAlnrDY9jP1gdCo+H0sD48CdXybblNwaYpwqC8VSpDdTndf',
+  '9j2wE/weihGp/DAdy/2kyBCaiOY1sjhUfJ1GogF49rC4jQRSYS9OAQQA6R/PtBFa',
+  'JaT4jq10yqASk4sqwVMsc6HcifM5lSdxzExFP74naUMMyEsKHP53QxTF0Grqusag',
+  'Qg/ZtgT0CN1HUM152y7ACOdp1giKjpMzOTQClqCoclyvWOFB+L/SwGEIJf7LSCEr',
+  'woBuJifJc8xAVr0XX0JthoW+uP91eTQ3XpsAEQEAAYkBPQQYAQIACQUCUmEvTgIb',
+  'LgCoCRBKY2E6TW5AlJ0gBBkBAgAGBQJSYS9OAAoJEOCE90RsICyXuqIEANmmiRCA',
+  'SF7YK7PvFkieJNwzeK0V3F2lGX+uu6Y3Q/Zxdtwc4xR+me/CSBmsURyXTO29OWhP',
+  'GLszPH9zSJU9BdDi6v0yNprmFPX/1Ng0Abn/sCkwetvjxC1YIvTLFwtUL/7v6NS2',
+  'bZpsUxRTg9+cSrMWWSNjiY9qUKajm1tuzPDZXAUEAMNmAN3xXN/Kjyvj2OK2ck0X',
+  'W748sl/tc3qiKPMJ+0AkMF7Pjhmh9nxqE9+QCEl7qinFqqBLjuzgUhBU4QlwX1GD',
+  'AtNTq6ihLMD5v1d82ZC7tNatdlDMGWnIdvEMCv2GZcuIqDQ9rXWs49e7tq1NncLY',
+  'hz3tYjKhoFTKEIq3y3Pp',
+  '=h/aX',
+  '-----END PGP PUBLIC KEY BLOCK-----'].join('\n');
+
+// Takes a Uint8Array encoded file and returns it encrypted as an Uint8Array
+// Right now we are encrypting file blobs, we want to look into using streams.
+async function encryptFile(input_file)
+{
+    var encrypted, options;
+    await openpgp.initWorker({ path:'openpgp.worker.js' });
+
+    options = {
+        message: openpgp.message.fromBinary(input_file),
+        publicKeys: (await openpgp.key.readArmored(pubkey)).keys,
+        armor: false
+    };
+
+    openpgp.encrypt(options).then(function(ciphertext) {
+        encrypted = ciphertext.message.packets.write(); // get raw encrypted packets as Uint8Array
+    });
+}
+
+function loadPublicKey(public_key) {
+    pubkey = public_key;
+}
+
+console.log(encryptFile(new Uint8Array([0x01, 0x01, 0x01])));
