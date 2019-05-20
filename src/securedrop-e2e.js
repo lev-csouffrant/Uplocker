63d4a1f6d8dfdf1a4521e0f56abf04e0798a3a44
Clint Miller <cmiller@leviathansecurity.com>
2019-05-20 14:44:18 -0700

    plug-in now reads a file and displays its contents in a sidebar.

--- src/securedrop-e2e.js
+++ src/securedrop-e2e.js
@@ -1,14 +1,23 @@
-function loadFile(o) 
-{
-	var fr = new FileReader();
-	reader.onload = function(e)
-		{	
-		showDataFile(e, o);
-		};
-	fr.readAsText(o.files[0]);
+function readSingleFile(e) {
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        displayContents(contents);
+    };
+    reader.readAsText(file);
+
 }
 
-function showDataFile(e, o)
-{
-	document.getElementById("data").innerText = e.target.result;
+function displayContents(contents) {
+    var element = document.getElementById('file-content');
+    element.textContent = contents;
 }
+
+document.getElementById('file-input')
+    .addEventListener('change', readSingleFile, false);
+
+
