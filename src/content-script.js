fb0c0a9cbfcd72dcfa0d4177b6fc4a3661db4a7e
clintmill <cmiller@leviathansecurity.com>
2019-05-24 09:09:47 -0700

    Modified the content script to capture file inputs with name fh.

--- src/content-script.js
+++ src/content-script.js
@@ -1,18 +1,25 @@
 var myPort = browser.runtime.connect({name:"port-from-cs"});
 
-var long_str = "AAAAAAAAA"
-var uInt8Array = new Uint8Array(1024 * 1024 * 32);
-for (var i = 0; i < uInt8Array.length; ++i) {
-    uInt8Array[i] = 0x41;
-}
-myPort.postMessage(uInt8Array.buffer, [uInt8Array.buffer]);
+
+//myPort.postMessage(uInt8Array.buffer, [uInt8Array.buffer]);
 //myPort.postMessage({greeting: long_str});
 
-myPort.onMessage.addListener(function(m) {
-    console.log("In content script, received message from background script: ");
-    console.log(m);
-});
 
-document.body.addEventListener("click", function() {
-    myPort.postMessage({greeting: "they clicked the page!"});
-});
+function readSingleFile(e) {
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+        var uInt8Array = new Uint8Array(e.target.result);
+        myPort.postMessage(uInt8Array.buffer, [uInt8Array.buffer]);
+    };
+
+    reader.readAsArrayBuffer(file);
+}
+
+// TODO: getElementsByName returns a list. Redo with type and listen on all input elements.
+const inputElement = document.getElementsByName("fh")[0]; 
+inputElement.addEventListener("change", readSingleFile, false);
